cc46ade8953891137b2e5ce1c56f0227f0986d93
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2024-02-13 12:06:37 +0530

    feat: add basic contract

---
+++ src/Constants.sol
@@ -0,0 +1,6 @@
+pragma solidity 0.8.21;
+
+abstract contract Constants {
+    string public constant NAME = "Joint";
+    string public constant SYMBOL = "JOINT";
+}
---
+++ src/Joint.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.8.21;
+
+import {Constants} from "./Constants.sol";
+import {DN404} from "dn404/src/DN404.sol";
+
+abstract contract Joint is DN404, Constants {
+
+}
